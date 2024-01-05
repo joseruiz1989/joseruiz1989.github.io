e44fb7c96e5d21ef1d602f9ef45ef05366de89c8
joseruiz1989 <joseruiz1989@hotmail.com>
2024-01-05 18:09:59 -0500

    actualizado mercado

--- js/codes/load_table_mercado.js
+++ js/codes/load_table_mercado.js
@@ -20,10 +20,28 @@ function imprimirMercado() {
                 for (let i = 1; i < lineas.length; i++) {
                     const valores = lineas[i].split(',');
                     const fila = tabla.insertRow(i);
-                    valores.forEach(valor => {
-                        const celda = fila.insertCell();
-                        celda.textContent = valor;
-                    });
+                    // valores.forEach(valor => {
+                    //     const celda = fila.insertCell();
+                    //     celda.textContent = valor;
+                    // });
+
+                    // console.log(valores);
+
+                    const celda0 = fila.insertCell();
+                    celda0.textContent = valores[0];
+
+                    const celda1 = fila.insertCell();
+                    celda1.textContent = valores[1];
+
+                    const celda2 = fila.insertCell();
+                    var numeroFormateado = Math.round(valores[2]);
+                    // console.log(numeroFormateado);
+                    var numeroFormateado1 = numeroFormateado.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    celda2.textContent = numeroFormateado1
+                    celda2.classList.add("precio_mercado");
+
+                    const celda3 = fila.insertCell();
+                    celda3.textContent = valores[3];
                 }
             })
             .catch(error => {
@@ -32,6 +50,16 @@ function imprimirMercado() {
     }
 
     llenarTablaDesdeCSV(urlArchivoCSV);
+
+
+    var numeroCells = document.querySelectorAll(".precio_mercado");
+
+    // Iterar sobre cada celda y formatear el número
+    numeroCells.forEach(function(cell) {
+        var numero = parseFloat(cell.innerHTML);
+        var numeroFormateado = numero.toLocaleString('es-ES', { minimumFractionDigits: 1, maximumFractionDigits: 1 });
+        cell.innerHTML = numeroFormateado;
+    });
 }
 
 function search_product() {
@@ -52,3 +80,6 @@ function search_product() {
       }       
     }
   }
+
+
+
---
+++ python_scripts/generar_valores_mercados.py
@@ -0,0 +1,240 @@
+# importing required modules
+import pandas as pd
+import glob
+
+import xml.etree.ElementTree as ET
+
+cac_ = r'{urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2}'
+cbc_ = r'{urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2}'
+cac_Attachment =        f'{cac_}Attachment'
+cac_ExternalReference = f'{cac_}ExternalReference'
+cbc_Description =       f'{cbc_}Description'
+cac_InvoiceLine =       f'{cac_}InvoiceLine'
+
+def get_main_root(xml_file):
+    prints = False
+    # Parsea el XML
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    # Accede a los elementos y sus valores
+    for elemento in root:
+        if elemento.tag == cac_Attachment:
+            if prints: print(f'{elemento.tag}: {elemento.text}')
+            for ele1 in elemento:
+                if prints: print(f'  {ele1.tag}: {ele1.text}')
+
+
+    if prints: print("-----------------------")
+
+    main_root = root.find(f'./{cac_Attachment}/{cac_ExternalReference}/{cbc_Description}')
+    # print(main_root.text)
+
+    return main_root
+
+def get_invoice_data(xml_path):
+    prints = False
+    main_root = get_main_root(xml_file)
+    
+    if prints: print("**************")
+    root = ET.fromstring(main_root.text)
+    # Accede a los elementos y sus valores
+    productos = []
+    
+    IssueDate = root.find(f'./{cbc_}IssueDate').text
+
+    for elemento in root:
+        if elemento.tag == cac_InvoiceLine:
+            producto = {}
+            if prints: print("------------------------------------------------------------------------------------------------------------------------------------------------")
+            # imprimir_elementos(elemento)
+            for el_01 in elemento:
+                if prints: print(f'{el_01.tag}: {el_01.text}')
+                
+            if prints: print("****************")
+
+            producto['IssueDate'] = IssueDate
+            
+            InvoicedQuantity = elemento.find(f'./{cbc_}InvoicedQuantity').text
+            producto['InvoicedQuantity'] = InvoicedQuantity
+            if prints: print(f'  InvoicedQuantity: {InvoicedQuantity}')
+
+            LineExtensionAmount = elemento.find(f'./{cbc_}LineExtensionAmount').text
+            producto['LineExtensionAmount'] = LineExtensionAmount
+            if prints: print(f'  LineExtensionAmount: {LineExtensionAmount}')
+
+            Item_Description = elemento.find(f'./{cac_}Item/{cbc_}Description').text
+            producto['Item_Description'] = Item_Description
+            if prints: print(f'  Item_Description: {Item_Description}')
+
+            Item_ID = elemento.find(f'./{cac_}Item/{cac_}StandardItemIdentification/{cbc_}ID').text
+            producto['Item_ID'] = Item_ID
+            if prints: print(f'  Item_ID: {Item_ID}')
+
+            Price_PriceAmount = elemento.find(f'./{cac_}Price/{cbc_}PriceAmount').text
+            producto['Price_PriceAmount'] = Price_PriceAmount
+            if prints: print(f'  Price_PriceAmount: {Price_PriceAmount}')
+
+            Price_BaseQuantity = elemento.find(f'./{cac_}Price/{cbc_}BaseQuantity').text
+            producto['Price_BaseQuantity'] = Price_BaseQuantity
+            if prints: print(f'  Price_BaseQuantity: {Price_BaseQuantity}')
+
+            
+            TaxTotal_Amount = elemento.find(f'./{cac_}TaxTotal/{cbc_}TaxAmount')
+            if TaxTotal_Amount != None:
+                TaxTotal_Amount = TaxTotal_Amount.text
+            else:
+                TaxTotal_Amount = 0
+            producto['TaxTotal_Amount'] = TaxTotal_Amount
+            if prints: print(f'  TaxTotal_Amount: {TaxTotal_Amount}')
+            
+            TaxTotal_RoundingAmount = elemento.find(f'./{cac_}TaxTotal/{cbc_}RoundingAmount')
+            if TaxTotal_RoundingAmount != None:
+                TaxTotal_RoundingAmount = TaxTotal_RoundingAmount.text
+            else:
+                TaxTotal_RoundingAmount = 0
+            # producto['TaxTotal_RoundingAmount'] = TaxTotal_RoundingAmount
+            if prints: print(f'  TaxTotal_RoundingAmount: {TaxTotal_RoundingAmount}')
+
+            TaxSubtotal_TaxableAmount = elemento.find(f'./{cac_}TaxTotal/{cac_}TaxSubtotal/{cbc_}TaxableAmount')
+            if TaxSubtotal_TaxableAmount != None:
+                TaxSubtotal_TaxableAmount = TaxSubtotal_TaxableAmount.text
+            else:
+                TaxSubtotal_TaxableAmount = 0
+            producto['TaxSubtotal_TaxableAmount'] = TaxSubtotal_TaxableAmount
+            if prints: print(f'  TaxSubtotal_TaxableAmount: {TaxSubtotal_TaxableAmount}')
+
+            TaxSubtotal_Category_Percent = elemento.find(f'./{cac_}TaxTotal/{cac_}TaxSubtotal/{cac_}TaxCategory/{cbc_}Percent')
+            if TaxSubtotal_Category_Percent != None:
+                TaxSubtotal_Category_Percent = TaxSubtotal_Category_Percent.text
+            else:
+                TaxSubtotal_Category_Percent = 0
+            producto['TaxSubtotal_Category_Percent'] = TaxSubtotal_Category_Percent
+            if prints: print(f'  TaxSubtotal_Category_Percent: {TaxSubtotal_Category_Percent}')
+
+            TaxSubtotal_Category_TaxScheme_ID = elemento.find(f'./{cac_}TaxTotal/{cac_}TaxSubtotal/{cac_}TaxCategory/{cac_}TaxScheme/{cbc_}ID')
+            if TaxSubtotal_Category_TaxScheme_ID != None:
+                TaxSubtotal_Category_TaxScheme_ID = TaxSubtotal_Category_TaxScheme_ID.text
+            else:
+                TaxSubtotal_Category_TaxScheme_ID = 0
+            producto['TaxSubtotal_Category_TaxScheme_ID'] = TaxSubtotal_Category_TaxScheme_ID
+            if prints: print(f'  TaxSubtotal_Category_TaxScheme_ID: {TaxSubtotal_Category_TaxScheme_ID}')
+
+            TaxSubtotal_Category_TaxScheme_Name = elemento.find(f'./{cac_}TaxTotal/{cac_}TaxSubtotal/{cac_}TaxCategory/{cac_}TaxScheme/{cbc_}Name')
+            if TaxSubtotal_Category_TaxScheme_Name != None:
+                TaxSubtotal_Category_TaxScheme_Name = TaxSubtotal_Category_TaxScheme_Name.text
+            else:
+                TaxSubtotal_Category_TaxScheme_Name = 0
+            # producto['TaxSubtotal_Category_TaxScheme_Name'] = TaxSubtotal_Category_TaxScheme_Name
+            if prints: print(f'  TaxSubtotal_Category_TaxScheme_Name: {TaxSubtotal_Category_TaxScheme_Name}')
+
+
+            Charge_Indicator = elemento.find(f'./{cac_}AllowanceCharge/{cbc_}ChargeIndicator')
+            if Charge_Indicator != None:
+                Charge_Indicator = Charge_Indicator.text
+            else:
+                Charge_Indicator = 0
+            producto['Charge_Indicator'] = Charge_Indicator
+            if prints: print(f'  Charge_Indicator: {Charge_Indicator}')
+
+            Charge_MultiplierFactorNumeric = elemento.find(f'./{cac_}AllowanceCharge/{cbc_}MultiplierFactorNumeric')
+            if Charge_MultiplierFactorNumeric != None:
+                Charge_MultiplierFactorNumeric = Charge_MultiplierFactorNumeric.text
+            else:
+                Charge_MultiplierFactorNumeric = 0
+            producto['Charge_MultiplierFactorNumeric'] = Charge_MultiplierFactorNumeric
+            if prints: print(f'  Charge_MultiplierFactorNumeric: {Charge_MultiplierFactorNumeric}')
+
+            Charge_Amount = elemento.find(f'./{cac_}AllowanceCharge/{cbc_}Amount')
+            if Charge_Amount != None:
+                Charge_Amount = Charge_Amount.text
+            else:
+                Charge_Amount = 0
+            producto['Charge_Amount'] = Charge_Amount
+            if prints: print(f'  Charge_Amount: {Charge_Amount}')
+
+            Charge_BaseAmount = elemento.find(f'./{cac_}AllowanceCharge/{cbc_}BaseAmount')
+            if Charge_BaseAmount != None:
+                Charge_BaseAmount = Charge_BaseAmount.text
+            else:
+                Charge_BaseAmount = 0
+            producto['Charge_BaseAmount'] = Charge_BaseAmount
+            if prints: print(f'  Charge_BaseAmount: {Charge_BaseAmount}')
+
+            producto['Precio_comparar'] = float(producto['Price_PriceAmount']) + float(producto['TaxTotal_Amount'])
+        
+            # if prints: print(f'{elemento.tag}: {elemento.text}')
+            productos.append(producto)
+
+    return productos
+
+def imprimir_elementos(elemento, nivel=0):
+    """Función recursiva para imprimir elementos XML."""
+    espacio = '  ' * nivel
+    tag = elemento.tag.replace(cac_, 'cac_').replace(cbc_, 'cbc_')
+    print(f"{espacio}{tag}")
+
+    for hijo in elemento:
+        imprimir_elementos(hijo, nivel + 1)
+
+    if elemento.text and elemento.text.strip():
+        print(f"{espacio}  Texto: |{elemento.text}|")
+
+
+if __name__ == '__main__':
+
+    mercados = [['Mercamio', '/home/joseruiz/Documents/mercados/mercamio/'],
+                ['Alkosto', '/home/joseruiz/Documents/mercados/alkosto/']]
+    
+    df = pd.DataFrame()
+    
+    for almacen, main_folder in mercados:
+        folders = glob.glob(f"{main_folder}*/")
+        folders.sort()
+        
+        for folder in folders[:]:
+            xml_files = glob.glob(f"{folder}*.xml")
+            for xml_file in xml_files:
+                print(f"{xml_file}")
+
+                productos = get_invoice_data(xml_file)
+                for prod in productos:
+                    prod['Almacen'] = almacen
+                    if '�' in prod['Item_Description']:
+                        prod['Item_Description'] = prod['Item_Description'].replace('�', 'n')
+                    # print("*/*/*/*/*/*/*/*/*/"*20)
+                    # for key in prod:
+                        # print(key, prod)
+                    df = df._append(prod, ignore_index=True)
+
+    df.fillna(0, inplace=True)
+    df['InvoicedQuantity'] = df['InvoicedQuantity'].astype(float)
+    df['Price_PriceAmount'] = df['Price_PriceAmount'].astype(float)
+    df['TaxTotal_Amount'] = df['TaxTotal_Amount'].astype(float)
+    df['Precio_comparar'] = df['Precio_comparar'].astype(int)
+
+    df['total']=round(df['InvoicedQuantity']*df['Price_PriceAmount'])
+    df['total_imp']=df['total']+df['TaxTotal_Amount']
+    
+    df = df.sort_values(by=['Item_Description', 'IssueDate'])
+
+    df['IssueDate'] = pd.to_datetime(df['IssueDate'])
+    df['IssueDate'] = df['IssueDate'].dt.strftime('%d %b %Y')
+    print(df.columns)
+
+    df_end = df[['IssueDate', 'Item_Description', 'Precio_comparar', 'Almacen']]
+
+    df_end = df_end.rename(columns={'IssueDate': 'Fecha',
+                                    'Item_Description': 'Producto',
+                                    'Precio_comparar': 'Precio'})
+    
+    
+
+    df_end.to_csv('files/mercado.csv', index=False)
+
+
+    print(df_end)
+
+            
+
+                
